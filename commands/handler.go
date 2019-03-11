0729263eb28bd90b08c2aadf67778d21360d95bf
Szymon Żogała <szymek.655@gmail.com>
2019-03-12 00:03:02 +0100

    Added a bunch of functionality and made several changes (including the function of the handler - it has a standalone function that will be hooked into discordgo

--- commands/handler.go
+++ commands/handler.go
@@ -1,24 +1,43 @@
 package commands
 
-import "strings"
+import (
+	comm "../communication"
+	"errors"
+	dg "github.com/bwmarrin/discordgo"
+	"strings"
+)
 
 // IllegalPrefix is the only prefix that cannot be used
 const IllegalPrefix = ""
 
 // registeredCommands contains all registered commands
-var registeredCommands = make(map[string]func([]string))
+var registeredCommands = make(map[string]func([]string) ([]string, error))
 
 // commandPrefix is the registered command prefix
 var commandPrefix string
 
-// ParseCommand attemts to parse an input string, if it's recognized as a command that command will be executed
-// Input is the message sent by a user, userID is the ID of the user that generated the message
-func ParseCommand(input, userID string) {
+// botID is the ID of the bot - used to ignore our own messages
+var botID string
 
-	if !isPrefixRegistered() {
-		// TODO: log prefix unregistered error
+// ParseCommand is a handler for incomming messages
+func ParseCommand(session *dg.Session, message *dg.MessageCreate) {
+
+	// Check if we're initialized
+	if ok, _ := isInitialized(); !ok {
+		// TODO: Log error - Handler not initialized
 		return
 	}
+
+	// Get the ID of the sender
+	userID := message.Author.ID
+
+	// Return if it's our message
+	if userID == botID {
+		return
+	}
+
+	// The message content
+	input := message.Content
 
 	// Check if the input string contains the registered prefix - if not, return as there is nothing we can do
 	if !strings.HasPrefix(input, commandPrefix) {
@@ -45,14 +64,16 @@ func ParseCommand(input, userID string) {
 		args := append([]string{userID}, slice[1:]...)
 
 		// If successful, log the event, execute the command and pass the remaining substrings
-		function(args)
+		if messages, err := function(args); err == nil {
+			comm.Send(session, message.ChannelID, messages)
+		}
 	}
 }
 
 // RegisterCommand registers command - assigns a specific function to a specific string.
 // When user types that string the assigned function will be executed.
 // DO NOT use any prefixes when registering functions.
-func RegisterCommand(name string, function func([]string)) bool {
+func RegisterCommand(name string, function func([]string) ([]string, error)) bool {
 
 	// Check if there's already a command registered for that name
 	if _, ok := registeredCommands[name]; ok {
@@ -81,7 +102,39 @@ func RegisterCommandPrefix(prefix string) bool {
 	return true
 }
 
-// Returns true if commandPrefix was correctly registered
+// RegisterBotID registers the given string as bot's ID
+// BotID can be registered only once. Future calls to this function won't do anything.
+// Returns true if registration was successful, false otherwise.
+func RegisterBotID(id string) bool {
+
+	// If there already is a botID defined
+	if botID == "" {
+		return false
+	}
+
+	// Otherwise assign the new value and return success
+	botID = id
+	return true
+}
+
+// isPrefixRegistered returns true if commandPrefix was correctly registered
 func isPrefixRegistered() bool {
 	return commandPrefix != IllegalPrefix
+}
+
+// isInitialized returns true if all necessary initialization was performed. If it's not the case returns false and assigns an error (based on first
+// caught unitialized aspect)
+func isInitialized() (bool, error) {
+
+	// Check if prefix was initialized
+	if !isPrefixRegistered() {
+		return false, errors.New("Handler error: prefix was not initialized")
+	}
+
+	// Check if botID was initialized
+	if botID == "" {
+		return false, errors.New("Handler error: botID was not initialized")
+	}
+
+	return true, nil
 }
