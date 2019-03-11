8d8eed3e1c7a3b84fa0214c5c4934cd14ac738a3
Szymon Żogała <szymek.655@gmail.com>
2019-03-11 19:44:22 +0100

    Added

---
+++ customtypes/config.go
@@ -0,0 +1,9 @@
+package customtypes
+
+// Config contains data necessary to configure the bot
+type Config struct {
+	// Token to use when connecting to the server
+	Token string
+	// Command prefix to use when detecting commands
+	CommandPrefix string
+}
