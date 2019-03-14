58e7414509da5f224411055670f399c14382fd6f
Szymon Żogała <szymek.655@gmail.com>
2019-03-14 23:11:45 +0100

    Removed no longer used code

--- MakroChatBot.go
+++ MakroChatBot.go
@@ -8,9 +8,6 @@ import (
 
 	"./initialization"
 	"./logger"
-	"./usercommands"
-
-	"github.com/bwmarrin/discordgo"
 )
 
 func main() {
@@ -35,54 +32,3 @@ func main() {
 
 	fmt.Printf("Finishing...")
 }
-
-// If error is not nil prints the message and error and panics
-func panicOnError(msg string, err error) {
-	if err != nil {
-		fmt.Printf("%s: %+v", msg, err)
-		panic(err)
-	}
-}
-
-// Handles incomming messages
-func messageHandler(discord *discordgo.Session, message *discordgo.MessageCreate) {
-
-	// User that sent the message
-	user := message.Author
-
-	// Don't respond to ourselves or other bots
-	if user.ID == "xd" || user.Bot {
-		return
-	}
-
-	// Simple echo for testing purposes
-	if message.Content == "xd123" {
-		discord.ChannelMessageSend(message.ChannelID, "xd123")
-	}
-
-	if message.Content == "!Roll" {
-
-		number, reaction := usercommands.Roll()
-
-		discord.ChannelMessageSend(message.ChannelID, fmt.Sprintf("%d", number))
-		discord.ChannelMessageSend(message.ChannelID, reaction)
-	}
-
-	if message.Content == "!Group1" {
-		discord.ChannelMessageSend(message.ChannelID, "https://i.kym-cdn.com/photos/images/facebook/001/290/942/c31.png")
-	}
-
-	if message.Content == "!Group2" {
-		discord.ChannelMessageSend(message.ChannelID, "https://vignette.wikia.nocookie.net/internet-meme/images/6/6e/Pogchamp.jpg/revision/latest?cb=20180310053228")
-	}
-}
-
-// Status update, maybe I'll do it later
-//discord.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
-//	err = discord.UpdateStatus(0, "A friendly helpful bot!")
-//	if err != nil {
-//		fmt.Println("Error attempting to set my status")
-//	}
-//	servers := discord.State.Guilds
-//	fmt.Printf("Running on %d servers", len(servers))
-//})
