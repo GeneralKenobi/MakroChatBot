23fc40ed1a37a975f9b3cf250b7b731b5842ebb2
Szymon Żogała <szymek.655@gmail.com>
2019-03-16 11:04:33 +0100

    Readability improvement

--- customtypes/config.go
+++ customtypes/config.go
@@ -2,8 +2,10 @@ package customtypes
 
 // Config contains data necessary to configure the bot
 type Config struct {
+
 	// Token to use when connecting to the server
 	Token string
-	// Command prefix to use when detecting commands
+
+	// Token to use when connecting to the server
 	CommandPrefix string
 }
